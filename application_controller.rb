cd40061a47ba375463bbb2e19dcfc8aabda1660c
Saber Khan <skhan@browning.edu>
2017-08-02 18:34:10 +0000

    basic birthstone

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 Bundler.require
+
 require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
@@ -7,7 +8,12 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
+  post '/results' do
+    @name = params[:user_name]
+    @month = params[:birth_month]
+    erb :results
+  end
   
   
 end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,34 @@
 def choose_gem(month)
-
+ #this method should look at the month
+ # and them return the correct stone
+ # elsewhere it should display the right image 
+ if month.downcase == "january"
+     return "Garnet"
+    elsif month.downcase == "february"
+        return "Amethyst"
+     elsif month.downcase == "march"
+        return "Aquamarine, Bloodstone"
+     elsif month.downcase == "april"
+        return "Diamond"
+    elsif month.downcase == "may"
+        return "Emrald"
+    elsif month.downcase == "june"
+        return "Pearl, Alexandrite, Moonstone"
+    elsif month.downcase == "july"
+        return "Ruby"
+    elsif month.downcase == "august"
+        return "Peridot"
+    elsif month.downcase == "september"
+        return "Sapphire"
+    elsif month.downcase == "october"
+        return "Tourmeline"
+    elsif month.downcase == "november"
+        return "Citrine, Yellow Topaz"
+    elsif month.downcase == "december"
+        return "Zircon, Turquoise"
+    else
+        return "didn't work, try again"
+    end 
  
 end
 
